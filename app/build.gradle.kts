be89183b90baaa4191639e33eb0f3f3e5946a6ff
Masoud <karimi.masoud5494@gmail.com>
2023-04-04 15:09:43 +0330

    use version catalog

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+
+}
+
+android {
+    namespace = "mkn.todo"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "mkn.todo"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            //proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Todo"
-include ':app'
+include(":app")
